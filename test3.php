e1206a2e732ce9bdbbe5c1765a8c1009acc7cdf8
makhome <qwerty7732t@ya.ru>
2023-08-24 23:49:44 +0300

    на первой страницуе один график и мин навторой есть

---
+++ test3.php
@@ -0,0 +1,49 @@
+<?php
+
+$mysqli = new mysqli('62.109.2.72', 'avtoparser', '7xXD2rN9i', 'avto1');
+if ($mysqli->connect_error) {
+    echo 'Failed to connect to MySQL: ' . $mysqli->connect_error;
+} else {
+    echo 'Connection established';
+}
+
+$sql=mysqli_query($mysqli,"SELECT DISTINCT name FROM avto1");
+
+$sql2=mysqli_query($mysqli,"SELECT *  FROM avto1 LIMIT 5 ");
+$result2 = mysqli_fetch_all($sql2, MYSQLI_ASSOC);
+
+
+// Здесь выполните ваш запрос с использованием $selectedName вместо жестко заданного значения 'VAG 04E-145299N'
+    $sql3 = mysqli_query($mysqli, "SELECT
+    MIN(price) AS min_price,
+    DATE(date) AS start_date
+FROM
+    avto1
+WHERE
+    name = 'BREMBO P85126X'
+GROUP BY
+    FLOOR(DATEDIFF(date, (SELECT MIN(date) FROM avto1 WHERE name = 'BREMBO P85126X')) / 3)
+ORDER BY
+    start_date;
+");
+
+    $result3 = mysqli_fetch_all($sql3, MYSQLI_ASSOC);
+
+foreach ($result3 as $row) {
+    echo "Start Date: " . $row['start_date'] . ", Min Price: " . $row['min_price'] . "<br>";
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+

ce8c387f1942cd6aa339181809c375d1944f1c6c
makhome <qwerty7732t@ya.ru>
2023-08-24 23:49:16 +0300

    на первой страницуе один график и мин навторой есть

--- script.js
+++ script.js
@@ -1,42 +1,4 @@
-// Получите данные из атрибутов data-*
-var canvas2 = document.getElementById('myChart2');
-var names2 = JSON.parse(canvas2.getAttribute('data-names'));
-var prices2 = JSON.parse(canvas2.getAttribute('data-prices'));
 
-// Создайте график
-var ctx2 = canvas2.getContext('2d');
-var myChart2 = new Chart(ctx2, {
-    type: 'bar',
-    data: {
-        labels: names2,
-        datasets: [{
-            label: 'Цена',
-            data: prices2,
-            backgroundColor: 'rgba(75, 192, 192, 0.6)',
-            borderColor: 'rgba(75, 192, 192, 1)',
-            borderWidth: 1
-        }]
-    },
-    options: {
-        legend: {
-            display: false
-        },
-        title: {
-            display: true,
-            text: 'Цена товаров',
-            position: 'top',
-            fontSize: 16,
-            padding: 20
-        },
-        scales: {
-            yAxes: [{
-                ticks: {
-                    beginAtZero: true
-                }
-            }]
-        }
-    }
-});
 
 // Создаем контекст для третьего графика
 var canvas3 = document.getElementById('myChart3');

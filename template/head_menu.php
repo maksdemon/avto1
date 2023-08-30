c41f754baff0333f820816c0e4a1dff74e987b77
makhome <qwerty7732t@ya.ru>
2023-08-30 21:58:25 +0300

    активный пункт

--- template/head_menu.php
+++ template/head_menu.php
@@ -1,16 +1,16 @@
         <nav>
           <ul class="nav nav-pills float-right">
             <li class="nav-item">
-              <a class="nav-link active" href="\">Home <span class="sr-only">(current)</span></a>
+              <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] === '/') echo "active"; ?>" href="\">Home <span class="sr-only">(current)</span></a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="\pie.php">pie</a>
+              <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] === '/pie.php') echo "active"; ?>" href="\pie.php">pie</a>
             </li>
             <li class="nav-item">
-              <a class="nav-link" href="\min.php">min</a>
+              <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] === '/min.php') echo "active"; ?> " href="\min.php" >min</a>
             </li>
               <li class="nav-item">
-                  <a class="nav-link" href="\add.php">add</a>
+                  <a class="nav-link <?php if ($_SERVER['REQUEST_URI'] === '/add.php') echo "active"; ?>" href="\add.php">add</a>
               </li>
               <li class="nav-item">
                   <span class="<?php echo ($isLessThan10Hours) ? 'green-circle' : 'red-circle'; ?>"></span>
